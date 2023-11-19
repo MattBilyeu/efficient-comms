1b6a4cf70bc06902c6d1286d2b5e407d88023d20
matt <53015609+MattBilyeu@users.noreply.github.com>
2023-11-19 18:48:54 -0500

    Initial Build
    Loaded packages and basics of the app.js file.

---
+++ app.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const mongoURI = require('./util/protected').mongoURI;
+const secret = require('./util/protected').secret;
+
+const app = express();
+
+const store = new MongoDBStore({
+    uri: mongoURI,
+    collection: 'sessions'
+})
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, 'public/files'))
+    },
+    filename: (req, file, cb)=> {
+        const timestamp = new Date().toISOString().replace(/:/g, '-');
+        cb(null, timestamp + '-' + file.originalname)
+    }
+});
+
+app.use(bodyParser.json());
+app(multer({storage: fileStorage}).single('pdf'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    session({
+        secret: secret,
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+
+mongoose
+    .connect(mongoURI)
+    .then(result => {
+        console.log('Connected');
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
