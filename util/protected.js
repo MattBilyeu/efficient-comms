63bfa19cbf82766f564bedd79afd66f871eb8e6b
matt <53015609+MattBilyeu@users.noreply.github.com>
2023-12-16 16:17:19 -0500

    Removed protected.js from repository

--- util/protected.js
+++
@@ -1,7 +0,0 @@
-exports.mongoURI = `mongodb+srv://Matt:${encodeURIComponent('DarthVader12')}@cluster0.g3qfov9.mongodb.net/efficient-comms`;
-
-exports.secret = "efficient-comms super secret password"
-
-exports.emailUser = 'matt.bilyeu1@gmail.com'
-
-exports.emailPassword = 'ptxs bchn fdaw muaz'
